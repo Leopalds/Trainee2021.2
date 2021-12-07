03dd9aafb171e7b6caf53e44f4dcac0dd8b819ee
dfreita4s <danifp19@gmail.com>
2021-12-07 14:38:11 -0300

    lol

--- ararashop/app/routes.php
+++ ararashop/app/routes.php
@@ -1,8 +1,11 @@
 <?php
+
+    use App\Core\Router;
+    
     $router->get('home', 'PagesController@home');
     $router->get('produtos', 'PagesController@produtos');
     $router->get('admProdutos', 'PagesController@admProdutos');
 
-    $router->post('produtosadm/create','PagesController@create');
-    $router->post('produtosadm/delete','PagesController@delete');
+    $router->post('admin/create','PagesController@create');
+    $router->post('admin/delete','PagesController@delete');
 ?>
