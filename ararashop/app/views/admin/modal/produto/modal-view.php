17eade2e287740f91e77fb3030f7209b300ce35b
dfreita4s <danifp19@gmail.com>
2021-12-11 13:39:55 -0300

    ta funcionando edit e add

--- ararashop/app/views/admin/modal/produto/modal-view.php
+++ ararashop/app/views/admin/modal/produto/modal-view.php
@@ -1,10 +1,10 @@
- <!-- Modal Vizualizar Produto -->
- <div class="modal fade modal-visualisar modais" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<!-- Modal Vizualizar Produto -->
+<div class="modal fade modais" tabindex="-1" id="modal-visualisar-<?= $itemProdutos->id ?>" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
 
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Moletom Selamento de Oito Trigramas</h5>
+          <h5 class="modal-title" id="exampleModalLabel"><?= $itemProdutos->nome ?></h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
             <span aria-hidden="true">&times;</span>
           </button>
@@ -14,16 +14,16 @@
             <div class="textview">
 
                 <p>DESCRIÇÃO:</p>
-                <p><?php $itemProdutos->descricao ?></p>
+                <p><?= $itemProdutos->descricao ?></p>
 
-                <p>Preço: R$ <?php number_format($itemProdutos->preco , 2, ',') ?> </p>
+                <p>Preço: R$ <?= number_format($itemProdutos->preco ,2,',') ?></p>
 
                 <p>CATEGORIA:</p>
-                <p><?php $itemProdutos->categoria ?></p>
+                <p><?= $itemProdutos->categoria ?></p>
 
             </div>
 
-            <img class="imgview"  src="<?php $itemProdutos->imagem ?>"/>
+            <img class="imgview" src="../../public/img/produtos/<?= $itemProdutos->imagem ?>">
 
       </div>
     </div>
