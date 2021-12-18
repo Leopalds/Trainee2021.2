bac630104628565801c47706b8e95bc91c1d60b8
dfreita4s <danifp19@gmail.com>
2021-12-18 18:38:06 -0300

    daniel odeia front end

--- ararashop/app/views/site/contato.view.php
+++ ararashop/app/views/site/contato.view.php
@@ -10,16 +10,17 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/7a3ef7191d.js" crossorigin="anonymous"></script>
-    <link href="../../public/css/contato.css" rel="stylesheet">
+    <link href="../../../public/css/contato.css" rel="stylesheet">
+    <link href="../../../public/css/navbarfooter.css" rel="stylesheet">
 
     <title>Contato</title>
 </head>
 
 <body>
-
+    <?php require 'navbar.php' ?>
     <div class="caminho">
         <ul>
-            <li><a href="home.html" style="color: #fbfbfb !important">Home</a></li>
+            <li><a href="/" style="color: #fbfbfb !important">Home</a></li>
             <li> > </li>
             <li>Login</li>
         </ul>
@@ -31,17 +32,17 @@
         <hr>
     </div>
 
-    <div class="container-fluid content-container">
+    <div class="content-container">
 
 
         <div class="contact">
             <h2 class="div-tittle">Alguma dúvida? Fale conosco!</h2>
             <form action="/contato/enviarEmail" method="POST">
 
-            <div class="row d-flex">
+                <div class="row d-flex">
                     <div class="form-group col-12">
                         <label for="formControlImputName">Nome</label>
-                        <input required type="text" name="nome"class="form-control"  id="nameFormInput" placeholder="Violet Evergarden">
+                        <input required type="text" name="nome" class="form-control" id="nameFormInput" placeholder="Violet Evergarden">
                     </div>
                     <div class="form-group col-12">
                         <label for="exampleFormControlInput1">Seu Email</label>
@@ -84,19 +85,17 @@
                 </li>
                 <br>
                 <li>
-                    <span class="d-inline-block">Tel: (32) 98868-1028</span></li>
+                    <span class="d-inline-block">Tel: (32) 98868-1028</span>
+                </li>
             </ul>
         </div>
-
-
-
     </div>
 
     <div class="container-map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.139460868239!2d-43.35196755009029!3d-21.77486288552834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989b69c914a28f%3A0xf85b3ff7e358eecf!2sBar%20do%20Zezinho!5e0!3m2!1spt-BR!2sbr!4v1635478808285!5m2!1spt-BR!2sbr"
-            allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.139460868239!2d-43.35196755009029!3d-21.77486288552834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989b69c914a28f%3A0xf85b3ff7e358eecf!2sBar%20do%20Zezinho!5e0!3m2!1spt-BR!2sbr!4v1635478808285!5m2!1spt-BR!2sbr" allowfullscreen></iframe>
     </div>
 
+    <?php require 'footer.php' ?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
