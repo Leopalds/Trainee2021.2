bacfdd4259b41f60f476d25228193bf7018a81c3
dfreita4s <danifp19@gmail.com>
2021-12-19 22:04:05 -0300

    dsadsada

--- ararashop/app/views/admin/viewadmcategorias.view.php
+++ ararashop/app/views/admin/viewadmcategorias.view.php
@@ -5,26 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../public/css/navbar-adm.css">
+    <script src="../../../public/js/navbarAdm.js"></script>
     
-    <link href="../../public/css/navbar-adm.css" rel="stylesheet">
-    <script src="../../public/js/navbarAdm.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/7a3ef7191d.js" crossorigin="anonymous"></script>
+    
+
 
     <link rel="stylesheet" href="../../../public/css/viewadmcategorias.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
-
 
     <title>Categorias ADM</title>
 </head>
 
 <body>
 
-    <?php require 'navbar.php'?>
+    <?php require 'navbar.php' ?>
 
     <div class="categorias-adm col-12">
 
@@ -71,7 +71,7 @@
                                 </td>
 
                             </tr>
-                            <?php endforeach; ?>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
@@ -79,19 +79,21 @@
 
             <nav class="d-flex justify-content-center" aria-label="Page navigation example">
                 <ul class="pagination">
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= 1?>'>Primeira</a></li>
-                    <?php if($pagina - 2 > 0){ ?>
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina - 2?>'><?= $pagina - 2?></a></li>
-                    <?php } if($pagina - 1 > 0){ ?>
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1?>'><?= $pagina - 1?></a></li>
+                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= 1 ?>'>Primeira</a></li>
+                    <?php if ($pagina - 2 > 0) { ?>
+                        <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina - 2 ?>'><?= $pagina - 2 ?></a></li>
+                    <?php }
+                    if ($pagina - 1 > 0) { ?>
+                        <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1 ?>'><?= $pagina - 1 ?></a></li>
                     <?php } ?>
-                    <li class='page-item active'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina?>'><?= $pagina?></a></li>
-                    <?php if($pagina + 1 <= $total_paginas){ ?>
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1?>'><?= $pagina + 1?></a></li>
-                    <?php } if($pagina + 2 <= $total_paginas){ ?>
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1?>'><?= $pagina + 2?></a></li>
+                    <li class='page-item active'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina ?>'><?= $pagina ?></a></li>
+                    <?php if ($pagina + 1 <= $total_paginas) { ?>
+                        <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1 ?>'><?= $pagina + 1 ?></a></li>
+                    <?php }
+                    if ($pagina + 2 <= $total_paginas) { ?>
+                        <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $pagina + 1 ?>'><?= $pagina + 2 ?></a></li>
                     <?php } ?>
-                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $total_paginas?>'>Última</a></li>
+                    <li class='page-item'><a class='page-link' href='/admin/categorias?pagina=<?= $total_paginas ?>'>Última</a></li>
                 </ul>
             </nav>
 
